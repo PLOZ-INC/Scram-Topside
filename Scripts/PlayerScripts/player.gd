1036c7769c6872f06dba2482d01df4aa270e45ec
Ciplin <83431101+Ciplin-The-Proto@users.noreply.github.com>
2026-03-25 23:42:51 -0600

    DASH MY WAY/ADDED A DASH
    ADDED DASHING!

--- Scripts/PlayerScripts/player.gd
+++ Scripts/PlayerScripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -600.0
+const Dash_Velocity = 1000
+var dash = false
+var dash_variable = true
 
 
 func _physics_process(delta: float) -> void:
@@ -13,13 +16,33 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	#Dashing controll
+	if Input.is_action_just_pressed("ctr-action") :
+		if dash_variable == true:
+			dash = true
+			dash_variable = false
+			$Dashing.start()
+			$Dash_Timer.start()
+			
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("a-move", "d-move")
 	if direction:
-		velocity.x = direction * SPEED
+		if dash == true:
+			velocity.x = direction * Dash_Velocity
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+#dash timer
+func _on_dashing_timeout() -> void:
+	dash = false
+
+
+func _on_dash_timer_timeout() -> void:
+	dash_variable = true
