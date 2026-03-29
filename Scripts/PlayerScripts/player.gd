8b5d14fc36bc358d4943a371e2b9d5c33eaaebae
Ciplin-Plozinc <ciplin@plozinc.com>
2026-03-28 21:00:09 -0600

    idk

--- Scripts/PlayerScripts/player.gd
+++ Scripts/PlayerScripts/player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("spacebar-move") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	#Dashing controll
